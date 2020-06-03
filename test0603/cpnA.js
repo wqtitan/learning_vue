2582557e92af47d3e883c7e99b5aa98f4361abd5
wqtitan <wqtitan@gmail.com>
2020-06-03 22:45:55 +0800

    v-for @ : 综合使用 点击标签变色

--- test0603/cpnA.js
+++ test0603/cpnA.js
@@ -3,20 +3,28 @@ import cpnB from "./cpnB.js"
 const cpnA = Vue.extend({
   methods: {
     repeatCpnB(fromb) {
-      
-      
       console.log("from cpnB: " + fromb)
+    },
+    itemclick(index) {
+      this.currentIndex = index;
+      
     }
   },
   template: `
     <div>
       <h2>cpnA say: {{ msgA }}</h2>
+      <ul>
+      <!-- 点击标签时变红 -->
+        <li v-for="(item, index) in movies" @click="itemclick(index)" :class="{active: currentIndex===index}">{{item}}</li>
+      </ul>
       <cpn-b :fromA="msgA" @cpnBClicked="repeatCpnB"></cpn-b>
     </div>
   `,
   data() {
     return {
       msgA: "I'm cpnA.",
+      movies: ["aaa", "bbb", "ccc"],
+      currentIndex: 0,
     }
   },
   provide: {
