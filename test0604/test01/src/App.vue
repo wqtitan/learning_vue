0a4e8260fab1516e97749848b16026b701a2b5cf
wqtitan <wqtitan@gmail.com>
2020-06-04 22:14:47 +0800

    tabbar

---
+++ test0604/test01/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <tab-bar>
+      <tab-bar-item path='/home' activeColor="#ff0">
+        <i slot="item-icon" class="el-icon-s-home"></i>
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path='/category'>
+        <i slot="item-icon" class="el-icon-menu"></i>
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path='/cart'>
+        <i slot="item-icon" class="el-icon-shopping-cart-2"></i>
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path='/profile'>
+        <i slot="item-icon" class="el-icon-s-custom"></i>
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from "./components/tabbar/TabBar";
+import TabBarItem from "./components/tabbar/TabBarItem";
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+export default {
+  name: "App",
+  components: {
+    TabBar,
+    TabBarItem
+  }
+}
+
+</script>
+<style>
+@import url(./assets/css/base.css);
+</style>
---
+++ test0604/test01/src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="tab-bar-item" :style="activeStyle" @click="itemClick">
+    <div><slot name="item-icon"></slot></div>
+    <div><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "TabBarItem",
+  methods: {
+    itemClick() {
+      this.$router.replace(this.path);
+    }
+  },
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: "red",
+    }
+  },
+  computed: {
+    isActive() {
+      return this.$route.path.indexOf(this.path) != -1;
+    },
+    activeStyle() {
+      return this.isActive ? {color: this.activeColor} : {}
+    }
+  }
+}
+</script>
+
+<style scoped>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+}
+
+.el-icon-house {
+  font-size: 24px;
+}
+</style>
