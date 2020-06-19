84367db4fb06a2dad25f85f5f2c31592265bad63
wqtitan <wqtitan@gmail.com>
2020-06-19 19:29:36 +0800

    add

---
+++ test0617/computed_test/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <watch-test></watch-test>
+  </div>
+</template>
+
+<script>
+import watchTest from "./components/WatchTest.vue";
+
+export default {
+  name: "App",
+  components: {
+    watchTest,
+  },
+};
+</script>
---
+++ test0617/computed_test/src/components/WatchTest.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    点击次数：{{ counter }}
+    <button @click="handleAdd()">+ 1</button>
+    <button @click="handleAdd(9999)">+ 10</button>
+  </div>
+</template>
+<script>
+import { setTimeout } from "timers";
+import { log } from "util";
+export default {
+  name: "watch",
+  data: function() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    handleAdd: function(count) {
+      console.log(event);
+      count = count || 1;
+      console.log(count);
+
+      // this指向当前 Vue实例app
+      this.counter += count;
+    },
+  },
+};
+</script>
